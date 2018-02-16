a380cf741e315ece2211a98ca447ff7d0b99123e
Kirakosian Aleksander <kair1998@gmail.com>
2018-02-16 18:10:28 +0300

    script was added

---
+++ script.sh
@@ -0,0 +1,27 @@
+bans=()
+
+for FILE in `ls`
+do
+    if test -d $FILE
+    then
+        skip=false
+        for BAN in ${bans[@]}
+        do
+            if [ "$BAN" == "$FILE" ]
+            then
+                skip=true
+            fi
+        done
+        if [ $skip == false ]
+        then
+            echo "$FILE"
+            cd "$FILE"
+            if gradle build
+            then
+                cd ../
+            else 
+                exit 1
+            fi
+        fi
+    fi
+done
