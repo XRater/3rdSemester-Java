3c78ec780355650bab098a29d604cc15d105784b
Kirakosian Aleksander <kair1998@gmail.com>
2018-03-07 01:10:55 +0300

    reformat code

--- ThreadPool/src/main/java/threadPool/LightFuture.java
+++ ThreadPool/src/main/java/threadPool/LightFuture.java
@@ -11,10 +11,10 @@ public interface LightFuture<T> extends Supplier<T> {
      * The method adds new task to the threadPool, which depends on the result of
      * the calculation. That means, that task cannot be evaluated until its parent task
      * is not ready.
-     *
+     * <p>
      * The method takes function f, and applies it to the result of the calculation.
      *
-     * @param f function to apply
+     * @param f   function to apply
      * @param <U> type of the function result
      * @return new LightFuture task, which was already added to the pool.
      */
