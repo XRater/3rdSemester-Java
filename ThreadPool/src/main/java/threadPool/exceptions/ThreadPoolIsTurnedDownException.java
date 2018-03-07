0c950c43eb17ead45887f6733f66e56a7349b284
Kirakosian Aleksander <kair1998@gmail.com>
2018-03-07 16:25:56 +0300

    All files are moved to packages

--- ThreadPool/src/main/java/threadPool/exceptions/ThreadPoolIsTurnedDownException.java
+++
@@ -1,8 +0,0 @@
-package threadPool.exceptions;
-
-/**
- * This exception is thrown in case of trying to add new task in the pool, that
- * was already turned off.
- */
-public class ThreadPoolIsTurnedDownException extends RuntimeException {
-}
