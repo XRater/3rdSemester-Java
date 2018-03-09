b326d44c35afff5e6c45dcb29b39bc061353dc8a
Kirakosian Aleksander <kair1998@gmail.com>
2018-03-09 15:45:45 +0300

    Tic-Tac-Toe reborn

--- HW2-Tic-Tac-Toe/src/test/java/MainTest.java
+++ HW2-Tic-Tac-Toe/src/test/java/MainTest.java
@@ -1,13 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
-public class MyTest {
+class MainTest {
 
     @Test
     void test() {
 //        fail("Fail");
     }
-
 
 }
